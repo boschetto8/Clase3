4f901bf471ad1dcedd47cc18f2a2cce83537f1d1
Octavio <octavio.boschetto@gmail.com>
2021-05-14 21:47:55 -0300

    subida tp

---
+++ Clase3 JS/script.js
@@ -0,0 +1,6 @@
+let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+let diasMes = ['31', '28', '31', '30','31','30','31','31','30','31','30','31']
+
+
+for (let i= 0; i < meses.length; i++){
+    console.log ('el mes de '+meses[i]+ ' tiene '+diasMes[i]+ ' cantidad de días') }
